44004887ece2e02a8773a13f6fdb8e2784ef1969
Silas Stilling-Jørgensen <sis002@edu.zealand.dk>
2024-09-18 09:54:36 +0200

    Add project files.

---
+++ TrophyLibrary/Trophy.cs
@@ -0,0 +1,35 @@
+﻿namespace TrophyLibrary
+{
+    public class Trophy
+    {
+        public int Id { get; set; }
+        public string? Competition { get; set; }
+        public int Year { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} {Competition} {Year}";
+
+        }
+
+        public void ValidateYear()
+        {
+            if (Year <= 1970 || Year >= 2024)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Year), "Year must be at least 1970");
+            }
+        }
+        public void ValidateCompetition()
+        {
+
+            if (Competition == null)
+            {
+                throw new ArgumentNullException(nameof(Competition), "Title cannot be null");
+            }
+            if (Competition.Length < 3)
+            {
+                throw new ArgumentException("Competition name must be at least 3 characters", nameof(Competition));
+            }
+        }
+    }
+}
---
+++ TrophyLibraryTests/TrophyTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrophyLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrophyLibrary.Tests
+{
+    [TestClass()]
+    public class TrophyTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValidateYearTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValidateCompetitionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
